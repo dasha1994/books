3fd103bc291650b8f301e08b805b0418d71d0e1a
dasha1994 <kozy.dasha@gmail.com>
2019-10-15 22:08:46 +0300

    new fields

--- src/main/kotlin/com/learning/kotlin/playground/books/domain/Book.kt
+++ src/main/kotlin/com/learning/kotlin/playground/books/domain/Book.kt
@@ -1,3 +1,3 @@
 package com.learning.kotlin.playground.books.domain
 
-data class Book(var id: String="", var title: String="")
+data class Book(var id: String, var title: String, var author: String, var genre: String, var year: Int)
