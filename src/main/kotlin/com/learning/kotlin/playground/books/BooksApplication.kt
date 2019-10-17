ae39796287d770bbe0315271e64d61a2140836ee
dasha1994 <kozy.dasha@gmail.com>
2019-10-17 21:37:56 +0300

    kotlin+spring boot

--- src/main/kotlin/com/learning/kotlin/playground/books/BooksApplication.kt
+++ src/main/kotlin/com/learning/kotlin/playground/books/BooksApplication.kt
@@ -1,13 +1,11 @@
 package com.learning.kotlin.playground.books
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class BooksApplication
 
 fun main(args: Array<String>) {
-	SpringApplication.run(BooksApplication::class.java, *args)
+	runApplication<BooksApplication>(*args)
 }
